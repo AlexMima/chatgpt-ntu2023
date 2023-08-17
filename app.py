1935309b4a6dc5160f1f340b59d44689848a32d5
AlexMima <131457905+AlexMima@users.noreply.github.com>
2023-08-18 00:17:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import openai
 from flask import Flask, render_template, request
 import os
 app = Flask(__name__)
-openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = "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"
 @app.route('/', methods=['GET', 'POST'])
 
 def index():
